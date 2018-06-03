9446c4ee15fe61306e8500646eb47c671b70776c
dutchpool <thamar-duikelaar@outlook.com>
2018-06-03 23:04:54 +0200

    new 0.9

--- docs/app.js
+++ docs/app.js
@@ -9,6 +9,24 @@ app.controller('indexCtrl', function($scope, $http) {
     $scope.orderByField = 'totalbalancedelta24h';
     $scope.reverseSort = true;
     $scope.searchCoin   = '';     // set the default search/filter term
+    $scope.dynamicPopover = {
+        content: 'Hello, World!',
+        templateUrl: 'myPopoverTemplate.html',
+        title: 'Title'
+      };
+
+
+    /**
+     * Encouragement controller for AngularJS
+     *
+     * @param $scope
+     * @param $http
+     * @param encouragementService
+     */
+      $scope.setUsername = function (username) {
+            $scope.userName = username;
+      };
+
 
     $http.get ('cdashboard.json').then (function (res) {
 		$scope.lastupdate = res.data.lasttimecalculated * 1000;
@@ -25,6 +43,7 @@ app.controller('indexCtrl', function($scope, $http) {
                 case 'dpos_delegate':
                     var it = res.data.coins[addr];
                     it['address'] = addr;
+                    $scope.dynamicPopover.content = "bla1"; //res.data.coins[addr]["notforgingdelegates"];
                     $scope.accounts_dpos.push (it);
                     break;
                 case 'dpos_private':
@@ -45,6 +64,7 @@ app.controller('indexCtrl', function($scope, $http) {
                     $scope.accounts_wallet.push (it);
                     break;
             }
-        }
+
+         }
     });
 });
